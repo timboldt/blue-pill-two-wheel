5bd64b9d464cf9485da55f8f5c23e9a036c1dab5
Tim Boldt <tim.boldt@gmail.com>
2019-06-24 22:56:41 -0700

    Further definition of OLED driver

--- src/oled.h
+++ src/oled.h
@@ -15,19 +15,31 @@
 #ifndef OLED_H
 #define OLED_H
 
+#include <stdbool.h>
 #include <stdint.h>
 
+#include "arm_math.h"
+
 // Writes out one row of text.
-// * If `text` contains more than 16 characters, it will be trucated.
-// * If `text` is less than 16 characters (there is a null character within the
+// - If `text` contains more than 16 characters, it will be trucated.
+// - If `text` is less than 16 characters (there is a null character within the
 // first 16 characters), the output will be padded with space until the end of
 // the line.
-// * `row` must be between 0 and 7.
-// * Returns true on success, and false if the device is busy.
+// - `row` must be between 0 and 7.
+// - Returns true on success, and false if the device is busy.
 bool OLED_SetText(uint8_t row, char *text);
 
 // Writes out one column of a plot.
-// TODO.
-bool OLED_PlotData(uint8_t top_row, uint8_t rows, int8_t *values)
+// - Internally, it maintains a column counter associated with `top_row` which
+// is incremented after every call to `OLED_PlotData`.
+// - If `reset` is true, the column is reset to zero.
+// - `top_row` must be between 0 and 7. (Each row is 8 pixels high - 64 pixels
+// in total.)
+// - The height of the graph will be `8 * rows` pixels.
+// - `top_row + rows` should be never be greater than 8.
+// - `rows` must be a multiple of 2, i.e. 1, 2, 4 or 8.
+// - `value` is a 8-bit signed value representing a number in the range [-1, 1).
+// Visually -1 is at the bottom and 1 is at the top.
+bool OLED_PlotData(bool reset, uint8_t top_row, uint8_t rows, q7_t value);
 
 #endif  // OLED_H
